f3747f1be3d2ce0298d0d954baf001cf2c007ef5
Aria Ketirani <122474903+Aketirani@users.noreply.github.com>
2023-02-22 20:14:27 +0100

    added plots

--- main.m
+++ main.m
@@ -3,28 +3,31 @@ clear, clc, clf % clear
 rng('default')  % generate the same random numbers
 
 % Load data
-load('../data/speech') % load speech
-load('../data/noise')  % load noise
-load('../data/bpir')   % load filter
+load('data/speech') % load speech
+load('data/noise')  % load noise
+load('data/bpir')   % load filter
 s = speech;            % speech x(n)
 x = noise;             % noise
 Pw = bpir;             % filter P(z)
 
 % Modes
-sim_mode = true;      % simulation mode
-optpara_mode = false; % optimize parameters mode
-ns_mode = false;      % noisy speech demo
+sim_mode = true;     % simulation mode
+optpara_mode = true; % optimize parameters mode
+ns_mode = true;      % noisy speech demo
+
+% Plot save
+plot_save = true;
+plot_path = 'plots/';
 
 % Initializate parameters
-
 T = 2000;   % iterations
 Nexp = 500; % experiments
 L = 10;     % filter length
 
 % Simulation Demo
 if sim_mode == true
-    Tmse = simulation(T, Nexp, L, Pw);
-    disp(Tmse);
+    Te = simulation(T, Nexp, L, Pw, plot_save, plot_path);
+    disp(Te);
 end
 
 % Optimize parameters Demo
@@ -35,7 +38,7 @@ if optpara_mode == true
     alg = 'LMS';            % algorithm
 
     % Optimize Parameters
-    [opt_L, opt_mu] = optpara(T, Nexp, L_vec, mu_vec, Pw, alg);
+    [opt_L, opt_mu] = optpara(T, Nexp, L_vec, mu_vec, Pw, alg, plot_save, plot_path);
     fprintf('Algorithm %s has optimal filter length L=%d\n', alg, opt_L);
     fprintf('Algorithm %s has optimal step size mu=%.2f\n', alg, opt_mu);
 end
@@ -43,6 +46,6 @@ end
 % Noisy speech signal Demo
 if ns_mode == true
     play = 'none'; % audio to play
-    Tmse = speechdemo(s, x, Pw, play);
-    disp(Tmse);
+    Te = noisyspeech(s, x, L, Pw, play, plot_save, plot_path);
+    disp(Te);
 end
--- src/simulation.m
+++ src/simulation.m
@@ -1,4 +1,4 @@
-function Tmse = simulation(T, Nexp, L, Pw)
+function Te = simulation(T, Nexp, L, Pw, plot_save, plot_path)
 % Simulates adaptive filters and evaluate their performance
 %
 % Inputs:
@@ -6,13 +6,20 @@ function Tmse = simulation(T, Nexp, L, Pw)
 %	Nexp: the number of experiments (positive integer)
 %   Pw: [Lx1] impulse response of the system
 %	L: [1x1] filter length (positive integer)
+%   plot_save: save the figure into a png if set to true (logical)
+%   plot_path: path to save the figure (string)
 %
 % Outputs:
-%	Tmse: [7x2] table containing the error for each adaptive filter algorithm
+%	Te: [7x2] table containing the error for each adaptive filter algorithm
 
 % Check inputs
+assert(nargin == 6, 'Invalid number of input arguments. The function requires 6 input arguments.')
 assert(isnumeric(T) && isscalar(T) && T > 0, 'T must be a positive scalar.')
 assert(isnumeric(Nexp) && isscalar(Nexp) && Nexp > 0, 'Nexp must be a positive scalar.')
+assert(isnumeric(L) && isscalar(L) && L > 0, 'L must be a positive scalar.')
+assert(isvector(Pw), 'Pw must be a vector.')
+assert(islogical(plot_save), 'plot_save must be a boolean value.');
+assert(ischar(plot_path), 'play must be a string.')
 
 % Initialize variables
 e = struct();
@@ -67,18 +74,22 @@ mse_fxrls = sum(eFxRLS,2)/Nexp;
 % Create table
 methods = {'W', 'LMS', 'NLMS', 'RLS', 'FxLMS', 'FxNLMS', 'FxRLS'};
 mse = [mse_w(end); mse_lms(end); mse_nlms(end); mse_rls(end); mse_fxlms(end); mse_fxnlms(end); mse_fxrls(end)];
-Tmse = table(methods', mse, 'VariableNames', {'Method', 'Error'});
+Te = table(methods', mse, 'VariableNames', {'Method', 'Error'});
 
 % Plot results
 figure(1)
-plot(1:T,10*log10(mse_w),'k',...
-     1:T,10*log10(mse_lms),'b',1:T,10*log10(mse_nlms),'r',...
-     1:T,10*log10(mse_rls),'g',1:T,10*log10(mse_fxlms),'c',...
-     1:T,10*log10(mse_fxnlms),'m',1:T,10*log10(mse_fxrls),'y')
+plot(1:T,10*log10(mse_w),'k',1:T,10*log10(mse_lms),'b',1:T,10*log10(mse_nlms),'r',1:T,10*log10(mse_rls),'g',...
+     1:T,10*log10(mse_fxlms),'c',1:T,10*log10(mse_fxnlms),'m',1:T,10*log10(mse_fxrls),'y')
 legend('W','LMS','NLMS','RLS','FxLMS','FxNLMS','FxRLS')
-title('Converge'); xlabel('Iterations'); ylabel('MSE (dB)')
+title('Performance'); xlabel('Iterations'); ylabel('MSE (dB)')
 figure(2)
 plot(1:T,d-yW,'k',1:T,d-yLMS,'b',1:T,d-yNLMS,'r',1:T,d-yRLS,'g',...
      1:T,d-yFxLMS,'c',1:T,d-yFxNLMS,'m',1:T,d-yFxRLS,'y')
 legend('W','LMS','NLMS','RLS','FxLMS','FxNLMS','FxRLS')
-title('Converge'); xlabel('Iterations'); ylabel('Error')
+title('Convergence'); xlabel('Iterations'); ylabel('Error')
+
+% Save figures to plot_path
+if plot_save == true
+    saveas(figure(1), [plot_path 'PerformanceSIM.png']);
+    saveas(figure(2), [plot_path 'ConvergenceSIM.png']);
+end
